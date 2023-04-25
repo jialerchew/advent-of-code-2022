61da860c4a618610c9bef51db7d9e9e0871076f5
unknown <chewjialer@gmail.com>
2023-04-26 02:49:37 +0800

    day1b

--- day01b/src/main.rs
+++ day01b/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+
+    let mut cals = include_str!("../input.txt")
+        .split("\n\n")
+        .map(|e| e.lines().filter_map(|c| c.parse::<i32>().ok()).sum::<i32>())
+        .collect::<Vec<i32>>();
+
+    cals.sort_unstable_by(|a, b| b.cmp(a));
+    println!(
+        "{}",
+        cals.iter()
+            .take(3)
+            .sum::<i32>()
+    )
+
 }
